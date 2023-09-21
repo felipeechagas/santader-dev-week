0fc8164f4d55860773037f8a7d84e17a28fee16e
leticiaslucena <leticiasantos.lucena@hotmail.com>
2023-09-20 22:34:03 -0300

    Projeto Java RESTful API criada para Santander Dev Week

---
+++ src/main/java/com/codever/SantaderDevWeekApplication.java
@@ -0,0 +1,13 @@
+package com.codever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantaderDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantaderDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codever/SantaderDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantaderDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
